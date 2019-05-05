fc8f95f67b4b7a38d692f44d36507f4ea8bb27dd
CapSkyHook <gautam.srinivasan9@gmail.com>
2019-05-05 17:10:14 -0400

    changes

--- data_parallel_rnn.py
+++ data_parallel_rnn.py
@@ -136,13 +136,13 @@ if torch.cuda.device_count()>1:
 
 for epoch in range(num_epochs):
     model.train()
+    hidden = model.module.init_hidden((args.batch_size * 2) / torch.cuda.device_count() )
     #linear annealing of learning rate at every 4th epoch
     if epoch%3==2:
         optimizer=torch.optim.Adam(model.module.parameters(), lr=learning_rate*0.5)
     for i, (data, lengths, unsort_idx, labels) in enumerate(train_loader):
         data_batch, length_batch, unsort_batch, label_batch = data.to(device), lengths.to(device),unsort_idx.to(device), labels.to(device)
         optimizer.zero_grad()
-        hidden = model.module.init_hidden((args.batch_size / torch.cuda.device_count()) * 2)
         import pdb; pdb.set_trace()
         hidden.view(2, -1, args.hidden_size_cnn)
         outputs, hidden = model(data_batch, hidden, length_batch, unsort_batch)
