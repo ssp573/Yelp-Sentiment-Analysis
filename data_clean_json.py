9c8f508a4a118a36463d640ec62ac82c58dac88f
CapSkyHook <gautam.srinivasan11@gmail.com>
2019-05-06 18:28:46 -0400

    changes

--- data_clean_json.py
+++ data_clean_json.py
@@ -21,23 +21,24 @@ business=pd.read_json('business.json', lines=True)
 # import pdb; pdb.set_trace()
 # business_filtered=business[~business['categories'].str.contains('Restaurant', na=False) & business['categories'].notnull()]
 
-df=pd.DataFrame(business_filtered, columns=['categories'])
-bar = df.categories.apply(lambda x: pd.value_counts(x.split(" "))).sum(axis = 0)
-bar.nlargest(10)
+#df=pd.DataFrame(business_filtered, columns=['categories'])
+#bar = df.categories.apply(lambda x: pd.value_counts(x.split(" "))).sum(axis = 0)
+#bar.nlargest(10)
 
-categories = ["Services,",
+categories = ["Services",
 "Home", 
-"Shopping,",
+"Shopping",
 "Beauty",  
 "Services", 
 "Health",   
-"Spas,",    
+"Spas",    
 "Hair",   
 "Local"]    
 
 for category in categories:
+    print(str(category))
     business_filtered=business[~business['categories'].str.contains('Restaurant', na=False) & business['categories'].str.contains(category, na=False) & business['categories'].notnull()]
-
+#    import pdb; pdb.set_trace()
 
     new_data=pd.merge(data[['text','stars','business_id']],business_filtered[['business_id','categories']],how='inner',on=['business_id'])
     new_data = data.filter(['text','stars'], axis=1)
@@ -45,10 +46,11 @@ for category in categories:
     new_data['sentiment'] = np.where(new_data['stars']>=4.0, 'pos', 'neg')
     pos_data=new_data[new_data['sentiment']=='pos']
     neg_data=new_data[new_data['sentiment']=='neg']
-    train_data=pd.concat([pos_data[:100000],neg_data[:100000]])
+    train_data=pd.concat([pos_data[:60000],neg_data[:60000]])
     # test_data=pd.concat([pos_data[100000:120000],neg_data[100000:1200000]])
     # val_data=pd.concat([pos_data[80000:],neg_data[80000:]])
     print(len(new_data))
+    print(len(train_data))
 
     train_data=train_data.drop('stars',axis=1)
     # val_data=val_data.drop('stars',axis=1)
