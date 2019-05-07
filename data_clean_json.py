6c0754266e450279ee544ff8c584c3b96f273310
CapSkyHook <gautam.srinivasan11@gmail.com>
2019-05-06 20:11:28 -0400

    updated data clean file

--- data_clean_json.py
+++ data_clean_json.py
@@ -47,26 +47,26 @@ for category in categories:
     pos_data=new_data[new_data['sentiment']=='pos']
     neg_data=new_data[new_data['sentiment']=='neg']
     train_data=pd.concat([pos_data[:60000],neg_data[:60000]])
-    # test_data=pd.concat([pos_data[100000:120000],neg_data[100000:1200000]])
+    test_data=pd.concat([pos_data[60000:80000],neg_data[60000:80000]])
     # val_data=pd.concat([pos_data[80000:],neg_data[80000:]])
     print(len(new_data))
     print(len(train_data))
 
     train_data=train_data.drop('stars',axis=1)
     # val_data=val_data.drop('stars',axis=1)
-    # test_data=test_data.drop('stars',axis=1)
+    test_data=test_data.drop('stars',axis=1)
 
     # print("Tokenizing val data...")
     # val_data['text']=val_data['text'].apply(tokenize)
     # val_data.to_pickle("data/val_{}_yelp_tokenized_no_stem.pkl".format(categories))
     # print("Val data saved at data/data/val_{}_yelp_tokenized_no_stem.pkl".format(categories))
-
+#    import pdb; pdb.set_trace()
     print("Tokenizing training data...")
     train_data['text']=train_data['text'].apply(tokenize)
-    train_data.to_pickle("data/train_{}_yelp_tokenized_no_stem.pkl".format(categories))
-    print("Train data saved at data/data/train_{}_yelp_tokenized_no_stem.pkl".format(categories))
+    train_data.to_pickle("data/train_{}_yelp_tokenized_no_stem.pkl".format(category))
+    print("Train data saved at data/train_{}_yelp_tokenized_no_stem.pkl".format(category))
 
-    # print("Tokenizing test data...")
-    # test_data['text']=test_data['text'].apply(tokenize)
-    # train_data.to_pickle("data/test_{}_yelp_tokenized_no_stem.pkl".format(categories))
-    # print("Train data saved at data/data/test_{}_yelp_tokenized_no_stem.pkl".format(categories))
+    print("Tokenizing test data...")
+    test_data['text']=test_data['text'].apply(tokenize)
+    train_data.to_pickle("data/test_{}_yelp_tokenized_no_stem.pkl".format(category))
+    print("Train data saved at data/test_{}_yelp_tokenized_no_stem.pkl".format(category))
