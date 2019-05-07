62b19220a807da80476e2b7a7389555769db1d18
CapSkyHook <gautam.srinivasan11@gmail.com>
2019-05-06 21:54:39 -0400

    update script

--- data_clean.s
+++ data_clean.s
@@ -2,12 +2,10 @@
 #BATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=40:00:00
-#SBATCH --mem=50GB
+#SBATCH --mem=20GB
 #SBATCH --job-name=data_clean
-#SBATCH --mail-type=END
-#SBATCH --mail-user=ssp573@nyu.edu
 #SBATCH --output=slurm_%j.out
-#SBATCH --cpus-per-task=28
+#SBATCH --cpus-per-task=4
 
 module purge
 module load python3/intel/3.6.3 cuda/9.0.176 nccl/cuda9.0/2.4.2
@@ -16,4 +14,4 @@ source /home/gs3011/pytorch_env/py3.6.3/bin/activate
 
 cd /scratch/gs3011/Yelp-Sentiment-Analysis
 
-python data_clean.py --category Services
+python data_clean_json.py --category Local
