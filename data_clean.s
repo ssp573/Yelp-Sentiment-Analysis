30e62c00efc3684e8385e4b73af067e0ebf5c028
CapSkyHook <gautam.srinivasan11@gmail.com>
2019-05-06 20:14:11 -0400

    added data cleaning job

---
+++ data_clean.s
@@ -0,0 +1,17 @@
+#!/bin/bash
+#BATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=40:00:00
+#SBATCH --mem=50GB
+#SBATCH --job-name=data_clean
+#SBATCH --mail-type=END
+#SBATCH --mail-user=ssp573@nyu.edu
+#SBATCH --output=slurm_%j.out
+#SBATCH --cpus-per-task=28
+
+module purge
+module load python3/intel/3.6.3 cuda/9.0.176 nccl/cuda9.0/2.4.2
+
+source /home/gs3011/pytorch_env/py3.6.3/bin/activate
+
+cd /scratch/gs3011/Yelp-Sentiment-Analysis
